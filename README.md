9ca1a009f67651c7e4d4c084bb5e12472222e2b4
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2019-07-30 16:12:45 +1000

    Implement credentials.Helper() interface

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # docker-credential-vault
 
 A [Docker credential helper](https://github.com/docker/docker-credential-helpers) to store credentials in [HashiCorp Vault](https://vaultproject.io).
+
+## KV setup
+
+The helper will require a KV backend configured in the Vault server.
+
+In order to configure the helper you will need to:
+
+ 1. `export DOCKER_CREDENTIAL_VAULT_KV_PATH=secret/path/to/use`: This is a KV backend path where the helper will store and look for credentials.
+ 1.  All the standard `$VAULT_` environment variables will be used to configured the vault client, e.g. `VAULT_ADDR`, `VAULT_TOKEN` etc.
+
+## Internal
+
+The helper will store the credentials in the following format, e.g. server URL is `https://example.com:8080`:
+
+`vault read -format=json $DOCKER_CREDENTIAL_VAULT_KV_PATH/example.com`
+
+That secret will contain the data body:
+
+```json
+{
+  "ServerURL": "https://example.com:8080",
+  "Username": "username",
+  "Secret": "secret"
+}
+```
--- helper.go
+++ helper.go
@@ -1,7 +1,12 @@
 package helper
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/docker/docker-credential-helpers/credentials"
+	"github.com/docker/docker-credential-helpers/registryurl"
 	"github.com/hashicorp/vault/api"
 )
 
@@ -15,17 +20,124 @@ func NewHelper(kvPath string, client *api.Client) *Helper {
 }
 
 func (h *Helper) Add(creds *credentials.Credentials) error {
-	return nil
+	client := h.client.Logical()
+	path, err := h.pathForServerURL(creds.ServerURL)
+	if err != nil {
+		return err
+	}
+	_, err = client.Write(path, map[string]interface{}{
+		"ServerURL": creds.ServerURL,
+		"Username":  creds.Username,
+		"Secret":    creds.Secret,
+	})
+	return err
 }
 
 func (h *Helper) Delete(serverURL string) error {
-	return nil
+	client := h.client.Logical()
+	path, err := h.pathForServerURL(serverURL)
+	if err != nil {
+		return err
+	}
+	_, err = client.Delete(path)
+	return err
 }
 
 func (h *Helper) Get(serverURL string) (string, string, error) {
-	return "", "", nil
+	creds, err := h.read(serverURL)
+	if err != nil {
+		return "", "", err
+	}
+	return creds.Username, creds.Secret, nil
 }
 
 func (h *Helper) List() (map[string]string, error) {
-	return nil, nil
+	client := h.client.Logical()
+
+	secret, err := client.List(h.kvPath)
+	if err != nil {
+		return nil, err
+	}
+
+	keys, ok := extractListData(secret)
+	if !ok {
+		return nil, fmt.Errorf("No credentials found at %s\n", h.kvPath)
+	}
+
+	var result map[string]string = make(map[string]string, 0)
+	for _, key := range keys {
+		server := key.(string)
+		if strings.HasSuffix(server, "/") {
+			// Skip any sub-paths, only looking for leaf keys
+			continue
+		}
+		creds, err := h.read(server)
+		if err != nil {
+			continue
+		}
+		result[creds.ServerURL] = creds.Username
+	}
+
+	return result, nil
+}
+
+// Internal helper to read a credential from vault
+func (h *Helper) read(serverURL string) (*credentials.Credentials, error) {
+	client := h.client.Logical()
+	path, err := h.pathForServerURL(serverURL)
+	if err != nil {
+		return nil, err
+	}
+	secret, err := client.Read(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read %s: %v\n", path, err)
+	}
+
+	return secretToCredential(secret)
+}
+
+func (h *Helper) pathForServerURL(serverURL string) (string, error) {
+	u, err := registryurl.Parse(serverURL)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s/%s", h.kvPath, registryurl.GetHostname(u)), nil
+}
+
+// Convert a Vault Secret to a Docker Credential
+func secretToCredential(secret *api.Secret) (*credentials.Credentials, error) {
+	server, exists := secret.Data["ServerURL"]
+	if !exists {
+		return nil, credentials.NewErrCredentialsMissingServerURL()
+	}
+	username, exists := secret.Data["Username"]
+	if !exists {
+		return nil, credentials.NewErrCredentialsMissingUsername()
+	}
+	password, exists := secret.Data["Secret"]
+	if !exists {
+		return nil, fmt.Errorf("no credentials secret")
+	}
+	return &credentials.Credentials{
+		ServerURL: server.(string),
+		Username:  username.(string),
+		Secret:    password.(string),
+	}, nil
+}
+
+// extractListData reads the secret and returns a typed list of data and a
+// boolean indicating whether the extraction was successful.
+func extractListData(secret *api.Secret) ([]interface{}, bool) {
+	if secret == nil || secret.Data == nil {
+		return nil, false
+	}
+
+	k, ok := secret.Data["keys"]
+	if !ok || k == nil {
+		return nil, false
+	}
+
+	i, ok := k.([]interface{})
+	return i, ok
 }
